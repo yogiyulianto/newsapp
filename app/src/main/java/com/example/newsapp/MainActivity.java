9d049d656c8f813e39a4b4ca015e9d44e14a8820
yogi <yogi@bringin.id>
2019-02-24 21:49:02 +0700

    add mainactivity sedikit

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -2,8 +2,26 @@ package com.example.newsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    String API_KEY = "0b7e92a424284aff9ac3a6ee5a5c4974";
+    String NEWS_SOURCE = "bbc-news";
+    ListView listView;
+    ProgressBar loader;
+
+    ArrayList<HashMap<String, String>> dataList = new ArrayList<HashMap<String, String>>();
+    static  final String KEY_AUTHOR = "author";
+    static final String KEY_TITLE = "title";
+    static final String KEY_DESCRIPTION = "description";
+    static final String KEY_URL = "url";
+    static final String KEY_URLTOIMAGE = "urlToImage";
+    static final String KEY_PUBLISHEDAT = "publishedAt"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
